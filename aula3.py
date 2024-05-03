3f9751fa6d0d41032c5d751b59e0f091c3922cd7
flaviann <168239056+flaviann@users.noreply.github.com>
2024-05-03 19:21:16 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,12 @@
+n1 =  int(input('>>'))
+n2 =  int(input('>>'))
+
+soma  =  n1 +  n2
+divisao  =  n1/n2
+mult =  n1 * n2
+sub =  n1 - n2
+
+print('Soma ', soma)
+print('Divisão ', divisao)
+print('Multiplicação ', mult)
+print('Subtração ', sub)
